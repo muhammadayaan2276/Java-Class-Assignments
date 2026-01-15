6d89aac09aec857556a478d9abb56425cf9ba55f
Ayaan Zeeshan <muhammad.ayaan.zeeshan123@gmail.com>
2026-01-15 19:17:07 +0500

    first commit

---
+++ PrintNumbers.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class PrintNumbers {
+    public static void main (String[] args){
+        int number;
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter a positive integer: ");
+        number = input.nextInt();
+        if(number <=0){
+            System.out.println("Enter a valid positive number");
+        } else{
+            int i = 1;
+            while(i <= number ){
+                System.out.println(i);
+                i++;
+
+            }
+        }
+        input.close();
+    }
+}
